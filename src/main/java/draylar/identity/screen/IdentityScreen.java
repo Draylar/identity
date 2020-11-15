30b1b1e63d5cd79d9d109956fbab15fa50e730cf
Draylar <samuelhimes@gmail.com>
2020-11-15 17:24:07 -0600

    port to vanilla UI

--- src/main/java/draylar/identity/screen/IdentityScreen.java
+++ src/main/java/draylar/identity/screen/IdentityScreen.java
@@ -4,48 +4,39 @@ import draylar.identity.cca.FavoriteIdentitiesComponent;
 import draylar.identity.cca.IdentityComponent;
 import draylar.identity.cca.UnlockedIdentitiesComponent;
 import draylar.identity.registry.Components;
-import draylar.identity.screen.widget.WEntityButton;
-import draylar.identity.screen.widget.WPlayerButton;
-import draylar.identity.screen.widget.WSearchBar;
-import draylar.identity.screen.widget.WVerticalScrollableUnfadingContainer;
+import draylar.identity.screen.widget.EntityWidget;
+import draylar.identity.screen.widget.PlayerWidget;
+import draylar.identity.screen.widget.SearchWidget;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import net.minecraft.client.util.Window;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.registry.Registry;
-import spinnery.client.screen.BaseScreen;
-import spinnery.widget.WAbstractWidget;
-import spinnery.widget.WInterface;
-import spinnery.widget.WStaticText;
-import spinnery.widget.WVerticalScrollableContainer;
-import spinnery.widget.api.Position;
-import spinnery.widget.api.Size;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class IdentityScreen extends BaseScreen {
+public class IdentityScreen extends Screen {
 
+    private final List<LivingEntity> unlocked;
     private final List<LivingEntity> renderEntities = new ArrayList<>();
-    private final List<WEntityButton> buttons = new ArrayList<>();
+    private final List<EntityWidget> entityWidgets = new ArrayList<>();
 
     public IdentityScreen() {
-        super();
+        super(new LiteralText(""));
 
-        // attempt to populate renderEntities list
         populateRenderEntities();
-
-        // base interface, scrollbar, and searchbar setup
-        WInterface wInterface = getInterface();
-        WVerticalScrollableUnfadingContainer container = createScrollableContainer(wInterface);
-        WSearchBar searchBar = createSearchBar(wInterface);
-
-        // add player icon next to searchbar
-        createPlayerButton(wInterface);
+        SearchWidget searchBar = addButton(createSearchBar());
+        addButton(createPlayerButton());
 
         // get identity components from player
         UnlockedIdentitiesComponent unlockedComponent = Components.UNLOCKED_IDENTITIES.get(MinecraftClient.getInstance().player);
@@ -53,7 +44,7 @@ public class IdentityScreen extends BaseScreen {
         IdentityComponent currentIdentityComponent = Components.CURRENT_IDENTITY.get(MinecraftClient.getInstance().player);
 
         // collect unlocked entities
-        List<LivingEntity> unlocked = collectUnlockedEntities(unlockedComponent);
+        unlocked = collectUnlockedEntities(unlockedComponent);
 
         // sort unlocked based on favorites
         Collections.sort(unlocked, (first, second) -> {
@@ -65,38 +56,82 @@ public class IdentityScreen extends BaseScreen {
         });
 
         // add entity widgets
-        populateEntityWidgets(container, unlocked, favoritesComponent, currentIdentityComponent);
-
-        // add hint if the player has no unlocks on what to do
-        if(unlocked.isEmpty()) {
-            TranslatableText message = new TranslatableText("identity.menu_hint");
-            float xPosition = (container.getWidth() / 2) - (MinecraftClient.getInstance().textRenderer.getWidth(message) / 2f);
-            float yPosition = (container.getHeight() / 2);
-            WStaticText hint = wInterface.createChild(WStaticText::new).setText(message).setPosition(Position.of(wInterface, xPosition, yPosition));
-        }
+        populateEntityWidgets(unlocked, favoritesComponent, currentIdentityComponent);
 
         // implement search handler
-        searchBar.setPostKeyPressListener(widget -> {
-            container.getWidgets().clear();
+        searchBar.setChangedListener(widget -> {
+            buttons.removeIf(button -> {
+                return button instanceof EntityWidget;
+            });
 
             List<LivingEntity> filtered = unlocked
                     .stream()
-                    .filter(livingEntity -> livingEntity.getType().getTranslationKey().contains(widget.getText()))
+                    .filter(livingEntity -> livingEntity.getType().getTranslationKey().contains(widget))
                     .collect(Collectors.toList());
 
-            populateEntityWidgets(container, filtered, favoritesComponent, currentIdentityComponent);
+            populateEntityWidgets(filtered, favoritesComponent, currentIdentityComponent);
         });
     }
 
-    private void populateEntityWidgets(WVerticalScrollableContainer container, List<LivingEntity> unlocked, FavoriteIdentitiesComponent favorites, IdentityComponent current) {
+    @Override
+    public void init(MinecraftClient client, int width, int height) {
+        this.client = client;
+        this.itemRenderer = client.getItemRenderer();
+        this.textRenderer = client.textRenderer;
+        this.width = width;
+        this.height = height;
+        this.setFocused(null);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        renderBackground(matrices);
+
+        // Render background hint when no identities have been collected
+        if(unlocked.isEmpty()) {
+            TranslatableText message = new TranslatableText("identity.menu_hint");
+            float xPosition = (getWindow().getWidth() / 2f) - (MinecraftClient.getInstance().textRenderer.getWidth(message) / 2f);
+            float yPosition = (getWindow().getHeight() / 2f);
+            MinecraftClient.getInstance().textRenderer.draw(matrices, message, xPosition, yPosition, 0xFFFFFF);
+        }
+
+        // tooltips
+        for (AbstractButtonWidget abstractButtonWidget : this.buttons) {
+            if (abstractButtonWidget.isHovered()) {
+                abstractButtonWidget.renderToolTip(matrices, mouseX, mouseY);
+                break;
+            }
+        }
+
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        float firstPos = entityWidgets.get(0).y;
+
+        // Top section should always have mobs, prevent scrolling the entire list down the screen
+        if(amount == 1 && firstPos >= 35) {
+            return false;
+        }
+
+        buttons.forEach(button -> {
+            if(button instanceof EntityWidget) {
+                button.y = (int) (button.y + amount * 10);
+            }
+        });
+
+
+        return false;
+    }
+
+    private void populateEntityWidgets(List<LivingEntity> unlocked, FavoriteIdentitiesComponent favorites, IdentityComponent current) {
         // add widget for each unlocked entity
-        int x = 0;
-        int y = 0;
+        int x = 15;
+        int y = 35;
         int rows = (int) Math.ceil(unlocked.size() / 7f);
 
         for(int yIndex = 0; yIndex <= rows; yIndex++) {
-            List<WEntityButton> widgets = new ArrayList<>();
-
             for(int xIndex = 0; xIndex < 7; xIndex++) {
                 int listIndex = yIndex * 7 + xIndex;
 
@@ -109,21 +144,21 @@ public class IdentityScreen extends BaseScreen {
                         isCurrent = true;
                     }
 
-                    // Add and create button
-                    WEntityButton button = new WEntityButton(livingEntity, this, favorites.has(livingEntity.getType()), isCurrent)
-                            .setSize(Size.of((getWindow().getScaledWidth() - 27) / 7f, getWindow().getScaledHeight() / 5f))
-                            .setPosition(Position.of(container, (getWindow().getScaledWidth() - 27) / 7f * x, getWindow().getScaledHeight() / 5f * y));
+                    EntityWidget entityWidget = new EntityWidget(
+                            (getWindow().getScaledWidth() - 27) / 7f * xIndex + x,
+                            getWindow().getScaledHeight() / 5f * yIndex + y,
+                            (getWindow().getScaledWidth() - 27) / 7f,
+                            getWindow().getScaledHeight() / 5f,
+                            livingEntity,
+                    this,
+                            favorites.has(livingEntity.getType()),
+                            isCurrent
+                    );
 
-                    buttons.add(button);
-
-//                    button.setOnAlign(widget -> widget.setSize(Size.of((getWindow().getScaledWidth() - 27) / 7f, getWindow().getScaledHeight() / 5f))
-//                            .setPosition(Position.of(container, (getWindow().getScaledWidth() - 27) / 7f * x, getWindow().getScaledHeight() / 5f * y)));
-
-                    widgets.add(button);
+                    addButton(entityWidget);
+                    entityWidgets.add(entityWidget);
                 }
             }
-
-            container.addRow(widgets.toArray(new WAbstractWidget[widgets.size()]));
         }
     }
 
@@ -153,41 +188,21 @@ public class IdentityScreen extends BaseScreen {
         return unlocked;
     }
 
-    private WVerticalScrollableUnfadingContainer createScrollableContainer(WInterface wInterface) {
-        WVerticalScrollableUnfadingContainer container = wInterface
-                .createChild(WVerticalScrollableUnfadingContainer::new)
-                .setSize(Size.of(getWindow().getScaledWidth(), getWindow().getScaledHeight() - 25))
-                .setPosition(Position.of(wInterface, 0, 25));
-
-        container.setOnAlign(widget -> widget.setSize(Size.of(getWindow().getScaledWidth(), getWindow().getScaledHeight() - 25))
-                .setPosition(Position.of(wInterface, 0, 25)));
-
-        return container;
+    private SearchWidget createSearchBar() {
+        return new SearchWidget(
+                getWindow().getScaledWidth() / 2f - (getWindow().getScaledWidth() / 4f / 2) - 5,
+                5,
+                getWindow().getScaledWidth() / 4f,
+                20f);
     }
 
-    private WSearchBar createSearchBar(WInterface wInterface) {
-        WSearchBar searchBar = wInterface
-                .createChild(WSearchBar::new)
-                .setSize(Size.of(getWindow().getScaledWidth() / 4f, 20f))
-                .setPosition(Position.of(getWindow().getScaledWidth() / 2f - (getWindow().getScaledWidth() / 4f / 2) - 5, 5, 0));
-
-        searchBar.setOnAlign(widget -> widget
-                .setSize(Size.of(getWindow().getScaledWidth() / 4f, 20f))
-                .setPosition(Position.of(getWindow().getScaledWidth() / 2f - (getWindow().getScaledWidth() / 4f / 2) - 5, 5, 0)));
-
-        return searchBar;
-    }
-
-    private WPlayerButton createPlayerButton(WInterface wInterface) {
-        WPlayerButton playerButton = wInterface
-                .createChild(() -> new WPlayerButton(this))
-                .setSize(Size.of(15f, 15f))
-                .setPosition(Position.of(getWindow().getScaledWidth() / 2f + (getWindow().getScaledWidth() / 8f) + 5, 7, 0));
-
-        playerButton.setOnAlign(widget -> widget
-                .setPosition(Position.of(getWindow().getScaledWidth() / 2f + (getWindow().getScaledWidth() / 8f) + 5, 7, 0)));
-
-        return playerButton;
+    private PlayerWidget createPlayerButton() {
+        return new PlayerWidget(
+                getWindow().getScaledWidth() / 2f + (getWindow().getScaledWidth() / 8f) + 5,
+                7,
+                15,
+                15,
+                this);
     }
 
     public Window getWindow() {
@@ -195,6 +210,11 @@ public class IdentityScreen extends BaseScreen {
     }
 
     public void disableAll() {
-        buttons.forEach(button -> button.setActive(false));
+        entityWidgets.forEach(button -> button.setActive(false));
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 }
---
+++ src/main/java/draylar/identity/screen/widget/EntityWidget.java
@@ -0,0 +1,117 @@
+package draylar.identity.screen.widget;
+
+import draylar.identity.Identity;
+import draylar.identity.network.ClientNetworking;
+import draylar.identity.screen.IdentityScreen;
+import draylar.identity.screen.ScreenUtils;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.widget.AbstractPressableButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.registry.Registry;
+
+public class EntityWidget extends AbstractPressableButtonWidget {
+
+    private final LivingEntity entity;
+    private final int size;
+    private boolean active;
+    private boolean starred;
+    private final IdentityScreen parent;
+
+    public EntityWidget(float x, float y, float width, float height, LivingEntity entity, IdentityScreen parent, boolean starred, boolean current) {
+        super((int) x, (int) y, (int) width, (int) height, new LiteralText("")); // int x, int y, int width, int height, message
+        this.entity = entity;
+        size = (int) (25 * (1 / (Math.max(entity.getHeight(), entity.getWidth()))));
+        entity.setGlowing(true);
+        this.parent = parent;
+        this.starred = starred;
+        this.active = current;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        boolean bl = mouseX >= (double)this.x && mouseX < (double)(this.x + this.width) && mouseY >= (double)this.y && mouseY < (double)(this.y + this.height);
+
+        if(bl) {
+            // Update current Identity
+            if(button == 0) {
+                PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
+                packet.writeIdentifier(Registry.ENTITY_TYPE.getId(entity.getType()));
+                ClientSidePacketRegistry.INSTANCE.sendToServer(ClientNetworking.IDENTITY_REQUEST, packet);
+                parent.disableAll();
+                active = true;
+            }
+
+            // Add to favorites
+            else if (button == 1) {
+                boolean favorite = false;
+
+                if(starred) {
+                    starred = false;
+                } else {
+                    starred = true;
+                    favorite = true;
+                }
+
+                // Update server with information on favorite
+                PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
+                packet.writeIdentifier(Registry.ENTITY_TYPE.getId(entity.getType()));
+                packet.writeBoolean(favorite);
+                ClientSidePacketRegistry.INSTANCE.sendToServer(ClientNetworking.FAVORITE_UPDATE, packet);
+
+                // TODO: re-sort screen?
+            }
+        }
+
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+
+        ScreenUtils.drawEntity(x + this.getWidth() / 2, (int) (y + this.getHeight() * .75f), size, -10, -10, entity, 15728880);
+
+        // Render selected outline
+        if(active) {
+            MinecraftClient.getInstance().getTextureManager().bindTexture(Identity.id("textures/gui/selected.png"));
+            DrawableHelper.drawTexture(matrices, x, y, getWidth(), getHeight(), 0, 0, 48, 32, 48, 32);
+        }
+
+        // Render favorite star
+        if(starred) {
+            MinecraftClient.getInstance().getTextureManager().bindTexture(Identity.id("textures/gui/star.png"));
+            DrawableHelper.drawTexture(matrices, x, y, 0, 0, 15, 15, 15, 15);
+        }
+
+        // Draw tooltip
+//        float x = MouseUtilities.mouseX;
+//        float y = MouseUtilities.mouseY;
+//
+//        if(getX() <= x && getX() + getWidth() >= x) {
+//            if(getY() <= y && getY() + getHeight() >= y) {
+//                drawTooltip(matrices, provider);
+//                renderToolTip();
+//            }
+//        }
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public void onPress() {
+
+    }
+}
