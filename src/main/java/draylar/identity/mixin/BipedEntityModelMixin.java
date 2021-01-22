66215fe6b2d43f58bb264733b86dbbf7c8f304e1
Draylar <samuelhimes@gmail.com>
2021-01-22 17:34:29 -0600

    fix husk/biped sneaking issue in #115

---
+++ src/main/java/draylar/identity/mixin/BipedEntityModelMixin.java
@@ -0,0 +1,42 @@
+package draylar.identity.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.BipedEntityRenderer;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.DrownedEntityModel;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.DrownedEntity;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MobEntityRenderer.class)
+public abstract class BipedEntityModelMixin extends LivingEntityRenderer {
+
+    public BipedEntityModelMixin(EntityRenderDispatcher dispatcher, EntityModel model, float shadowRadius) {
+        super(dispatcher, model, shadowRadius);
+    }
+
+    @Inject(
+            method = "render",
+            at = @At("HEAD"))
+    private void onRender(MobEntity mobEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        // Only apply to Biped Entities
+        if (!((Object) this instanceof BipedEntityRenderer)) {
+            return;
+        }
+
+        BipedEntityModel<?> model = (BipedEntityModel) getModel();
+
+        if (model != null) {
+            model.sneaking = mobEntity.isInSneakingPose();
+        }
+    }
+}
